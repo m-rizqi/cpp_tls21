6bab8d98a048d51945333723c8f65c11a5920258
Muhammad Rizqi <mrizqi070502@gmail.com>
2021-10-06 23:10:54 +0700

    create testing-form

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,13 @@ using namespace std;
 void set_cursor_click(int x, int y)
 {
     SetCursorPos(x, y);
-    Sleep(300);
-    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
-    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    Sleep(100);
+    INPUT input[2];
+    input[0].type = MOUSE_EVENT;
+    input[0].ki.dwFlags = MOUSEEVENTF_LEFTDOWN;
+    input[1].type = MOUSE_EVENT;
+    input[1].ki.dwFlags = MOUSEEVENTF_LEFTUP;
+    SendInput(2, input, sizeof(INPUT));
 }
 
 void scroll(int n)
@@ -58,18 +62,14 @@ void get_coordinate()
 
 void send_key(int key)
 {
-    INPUT input;
-    WORD vkey = key;
-    input.type = INPUT_KEYBOARD;
-    input.ki.wScan = MapVirtualKey(vkey, MAPVK_VK_TO_VSC);
-    input.ki.time = 0;
-    input.ki.dwExtraInfo = 0;
-    input.ki.wVk = vkey;
-    input.ki.dwFlags = 0;
-    SendInput(1, &input, sizeof(INPUT));
-    input.ki.wVk = key;
-    input.ki.dwFlags = KEYEVENTF_KEYUP;
-    SendInput(1, &input, sizeof(INPUT));
+    INPUT input[2];
+    input[0].type = INPUT_KEYBOARD;
+    input[0].ki.wVk = key;
+    input[0].ki.dwFlags = KEYEVENTF_EXTENDEDKEY;
+    input[1].type = INPUT_KEYBOARD;
+    input[1].ki.wVk = key;
+    input[1].ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(2, input, sizeof(INPUT));
 }
 
 void fill_niu()
@@ -78,7 +78,7 @@ void fill_niu()
     for (int i = 0; i < 6; i++)
     {
         send_key(niu[i]);
-        Sleep(700);
+        Sleep(100);
     }
 }
 
@@ -139,77 +139,125 @@ void take_screenschot()
     keybd_event(VK_LWIN, 0, KEYEVENTF_KEYUP, 0);
 }
 
+void ctrl_save()
+{
+    INPUT ip;
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.wScan = 0;
+    ip.ki.time = 0;
+    ip.ki.dwExtraInfo = 0;
+    ip.ki.wVk = VK_CONTROL;
+    ip.ki.dwFlags = 0;
+    SendInput(1, &ip, sizeof(INPUT));
+    ip.ki.wVk = 0x53;
+    ip.ki.dwFlags = 0;
+    SendInput(1, &ip, sizeof(INPUT));
+    ip.ki.wVk = 0x53;
+    ip.ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, &ip, sizeof(INPUT));
+    ip.ki.wVk = VK_CONTROL;
+    ip.ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, &ip, sizeof(INPUT));
+}
+void fill_image_name()
+{
+    uppering_word('P');
+    Sleep(100);
+    send_key(0x31);
+    Sleep(100);
+    send_key(0x33);
+}
 int main()
 {
-    system("start https://zoom.us/j/93843429498?pwd=VVR4UjlGejQwWkNhVFNVZ1FKb0ZUUT09");
-    Sleep(10000);
-    send_key(0x09);
-    Sleep(500);
-    send_key(0x09);
-    Sleep(500);
-    send_key(0x09);
-    Sleep(500);
-    send_key(0x0D);
-    Sleep(15000);
-    send_key(0x09);
-    Sleep(500);
-    send_key(0x09);
-    Sleep(500);
-    send_key(0x09);
-    Sleep(500);
-    send_key(0x0D);
-    Sleep(500);
-    maximize_window();
-    SetCursorPos(400, 100);
-    mouse_event(MOUSEEVENTF_LEFTDOWN, 400, 100, 0, 0);
-    send_key(0x09);
-    Sleep(100);
-    send_key(0x09);
-    Sleep(100);
-    send_key(0x09);
-    Sleep(100);
-    send_key(0x0D);
-    Sleep(100);
-    send_key(0x09);
-    Sleep(100);
-    send_key(0x09);
-    Sleep(100);
-    send_key(0x09);
-    Sleep(100);
-    send_key(0x09);
-    Sleep(100);
-    send_key(0x0D);
-    Sleep(100);
-    take_screenschot();
-    set_cursor_click(1145,555);
-    // Sleep(700);
-    // set_cursor_click(504,353);
-    // Sleep(200);
+    // system("start https://zoom.us/j/93843429498?pwd=VVR4UjlGejQwWkNhVFNVZ1FKb0ZUUT09");
+    // Sleep(7000);
+    // send_key(0x09);
+    // Sleep(100);
+    // send_key(0x09);
+    // Sleep(100);
+    // send_key(0x09);
+    // Sleep(100);
+    // send_key(0x0D);
+    // Sleep(15000);
+    // send_key(0x09);
+    // Sleep(500);
     // send_key(0x09);
     // Sleep(500);
     // send_key(0x09);
     // Sleep(500);
     // send_key(0x0D);
-    int p = 8;
-    // cin >> p;
-    // system("start https://forms.gle/LQ5KL7XgZN7hrAKRA");
-    // Sleep(7000);
-    // send_key(0x09);
     // Sleep(500);
+    // maximize_window();
+    // SetCursorPos(400, 100);
+    // mouse_event(MOUSEEVENTF_LEFTDOWN, 400, 100, 0, 0);
     // send_key(0x09);
-
-    // Sleep(500);
+    // Sleep(100);
     // send_key(0x09);
+    // Sleep(100);
+    // send_key(0x09);
+    // Sleep(100);
+    // send_key(0x0D);
+    // Sleep(100);
+    // send_key(0x09);
+    // Sleep(100);
+    // send_key(0x09);
+    // Sleep(100);
+    // send_key(0x09);
+    // Sleep(100);
+    // send_key(0x09);
+    // Sleep(100);
+    // send_key(0x0D);
+    // Sleep(100);
+    // take_screenschot();
+    // while(1){
+    //     if(GetKeyState(0x0D) & 0x8000){
+    //         break;
+    //     }
+    // }
     // Sleep(1000);
-    // fill_niu();
-    // send_key(0x09);
-    // fill_name();
-    // send_key(0x09);
-    // for (int i = 0; i < p; i++)
+    // set_cursor_click(1200,570);
+    // while (1)
     // {
-    //     send_key(0x28);
+    //     if (GetKeyState(0x0D) & 0x8000)
+    //     {
+    //         break;
+    //     }
     // }
     // send_key(0x09);
     // send_key(0x09);
+    // send_key(0x09);
+    // send_key(0x09);
+    // send_key(0x0D);
+    // while(1){
+    //     if(GetKeyState(0x08)){
+    //         break;
+    //     }
+    // }
+    // fill_image_name();
+    // Sleep(100);
+    // send_key(0x09);
+    // Sleep(100);
+    // send_key(0x09);
+    // Sleep(100);
+    // send_key(0x0D);
+    system("start https://forms.gle/L93FWKwDZjcDiBGs5");
+    Sleep(7000);
+    set_cursor_click(300,300);
+    send_key(0x09);
+    Sleep(100);
+    send_key(0x09);
+    Sleep(100);
+    send_key(0x09);
+    Sleep(1000);
+    fill_niu();
+    send_key(0x09);
+    fill_name();
+    send_key(0x09);
+    for (int i = 0; i < 13; i++)
+    {
+        send_key(0x28);
+    }
+    send_key(0x09);
+    send_key(0x09);
     return 0;
 }
